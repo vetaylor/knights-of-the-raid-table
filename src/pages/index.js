5d7a01709386a507620090b40a165c2bd891ca9a
Victoria Taylor <taylor.victoriae@gmail.com>
2018-09-26 23:56:38 -0500

    💖 cleaned up starter site codebase

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,15 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>An exiting new adventure awaits...</h1>
+    <p>
+      <strong>His scowl</strong> serves as a stark contrast to the tender affection Gunzy's <u style={{ textDecorationColor: 'green'}}>supple</u> fingers were showing his feline companion.
+    Waiting at the bus stop for the “specialist” he summoned to “court”,
+    "How dare he make the king wait?" yells the self-proclaimed king of
+    All-Over-The-Place.
+    </p>
+    <p>
+    ...
+    </p>
   </Layout>
 )
 
