741b79cd13045ededbafaeef209e9d277ac2c2c5
Victoria Taylor <vtaylor@localhost.localdomain>
2018-09-28 14:11:05 -0500

    added text for 9/24-9/28/18

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,13 +5,132 @@ import Layout from '../components/layout'
 const IndexPage = () => (
   <Layout>
     <p>
-      <strong>His scowl</strong> serves as a stark contrast to the tender affection Gunzy's <u style={{ textDecorationColor: 'green'}}>supple</u> fingers were showing his feline companion.
-    Waiting at the bus stop for the “specialist” he summoned to “court”,
-    "How dare he make the king wait?" yells the self-proclaimed king of
-    All-Over-The-Place.
+      His scowl serves as a stark contrast to the tender affection
+      Gunzy's <span style={{ textDecoration: 'underline #9be835'}}>supple</span> fingers
+      were showing his feline companion. Waiting at the bus stop for the
+      “specialist” he summoned to “court”,
+      "How dare he make the king wait?" yells the self-proclaimed king of
+      All-Over-The-Place.
     </p>
     <p>
-    ...
+      Exhausted from pacing, he sits to stroke Catrick. “Of all my feline
+      companions, you're my favorite Mr. Catrick Swayze,” Gunzy imagines running
+      his fingers through Catrick’s fluffy mane. Catrick is a sphynx. For the
+      moment. “No one loves daddy like you do. Yes you love daddy don't you?”
+    </p>
+    <p>
+      The residue of his altercation at Planet Fitness earlier in the day lingers.
+      "I'm so outraged at that gym attendant at Planet Fitness! How dare
+      he <span style={{ textDecoration: 'underline #ff9438'}}>ostracize</span> me
+      from my place of body worship!," he reflects. Catrick spontaneously erupts
+      into a Persian. Gunzy is delighted. He likes it when Catrick becomes a persian.
+      He continues muttering to Catrick, “So what if I indulged myself to a slice
+      of pizza while revelling at myself on the thigh machine, bloody communists.”
+    </p>
+    <p>
+      ...
+    </p>
+    <p>
+      Earlier in the week Gunzy embarked on a VHS frenzy. He fingered and prodded
+      boxes packed full of video tapes until he settled on a Three's Company binge.
+      He determined it critical to examine what he believed to be the subtle sexual
+      tension between Don Knotts and Suzanne Somers. He must decode the conspiracy.
+      It was then he became inspired to make good on his 1991 New Year Resolution
+      to "execute a workout routine". The sticky note was still there on the dusty
+      tape marked: Wholesome Animal Sounds and Suggestive Exercise Commercials.
+    </p>
+    <p>
+      "I dub it Project Thick Thighs!" he celebrated then proceeded to
+      parsimoniously mimic, into a mirror, the salacious thigh squeezing movements
+      of the Thighmaster Toning System. "Practice makes perfect," he chuckled,
+      camera in hand, Catrick pacing in and out of the field of view. He later
+      marked this tape: Patriot Thighs That Will Make America Great Again.
+      Catrick was a Siamese at the time.
+    </p>
+    <p>
+      Since project launch, Gunzy has, "been removed from the gym at least three
+      times," Jenny commented to Austen, the head bro at Planet Fitness. She
+      slapped him playfully and with a toothy grin she continued, "Why do you keep
+      letting him in here?" It's the kind of toothy grin that implies sex in a
+      public bathroom.
+    </p>
+    <p>
+      Austen flexed his bicep and imagined a steaming plate of corned beef hash,
+      "He's hilarious. Didn't you see him there at the thigh master feeding his
+      cat nibbles of cheese pizza? I let him stay until there is a complaint."
+    </p>
+    <p>
+      ...
+    </p>
+    <p>
+      The bus squeals to a stop and spits out Snitch, Gunzy's "specialist". "It's
+      about time <span style={{ textDecoration: 'underline #ffe32c'}}>manservant</span>.
+      You make me sit idle at your peril."
+    </p>
+    <p>
+      "I apologize sire! I am at the mercy of the public transportation. The driver
+      did not heed my pleas!" Snitch raged. The driver slams the folding door shut
+      and zooms away.
+    </p>
+    <p>
+      "Excuses!"
+    </p>
+    <p>
+      Snitch bows low and inquires, "Everything okay sire?"
+    </p>
+    <p>
+      Vigorously massaging his thighs, Gunzy retorts, "I am sore from my workout.
+      I did three sets of," he thinks of an amount of weight that might impress
+      Snitch, "five hundred pounds."
+    </p>
+    <p>
+      "Wow sire, I'm­—"
+    </p>
+    <p>
+      "Pray," Gunzy inserts waving his hand dismissively satisfied, "what did
+      learn from your meeting with the soothsayer?"
+    </p>
+    <p>
+      "Behold!", Snitch ejaculated with a flourish, and a gay little bow.
+      "The soothsayer sayeth, and you will heed. The earth is
+      indeed <span style={{ textDecoration: 'underline #e85981'}}>spherical</span>,
+      not flat."
+    </p>
+    <p>
+      "Balderdash!", came the expected retort. "That can not be true! You'd fall
+      off the planet at night if that was so."
+    </p>
+    <p>
+      "But great he-who-shall-be-obeyed!", cries Snitch. "I only do your bidding
+      ever since you saved me from myself that time when I almost did that thing."
+    </p>
+    <p>
+      Great drops of tears fall from Snitch's face. Pleadingly, "You remember that
+      thing don't you? That thing I almost did."
+    </p>
+    <p>
+      Gunzy slowly shifts his gaze to the left then to the right, up, down and
+      once around clockwise for good luck. Clearly not recalling Snitch’s plight,
+      his voice wavers as he hesitantly answers, “yes...?”
+    </p>
+    <p>
+      Relief washes over Snitch as his faith in his employer is restored stimulating
+      a sense of self-righteousness. Gunzy scratches his head visibly disturbed by
+      Snitch. I've got to stop hiring these discount cronies off Craigslist, he
+      thinks. <em>I tasked him to uncover the truth of the prophecy and he comes back
+        to me with the earth is rou­—</em>
+    </p>
+    <p>
+      Sudden realization pierces the King like an arrow through the heart.
+      Clutching his chest, he drops Catrick who lets out a shrill "mrrrow" in
+      protest. His face transitions from shock to fear to utter fury.
+    </p>
+    <p>
+      "Spherical! " Gunzy spits unable to contain his disgust. "If what this so-called
+      soothsayer asserts is true, who knows what's out there? Imagine the
+      delinquents tramping around outside my <span style={{ textDecoration: 'underline #3affa'}}>bailiwick</span>!"
+      He lets out a scornful laugh. "No, I can't have that. I want to see this
+      soothsayer myself."
     </p>
   </Layout>
 )
