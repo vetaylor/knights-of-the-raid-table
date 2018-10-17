fb20236add3c169b42ca689eb5b2702f9ef1bc8e
naelotus <nate.w.peek@gmail.com>
2018-10-16 20:09:52 -0500

    added naelotus bio to authors.js

--- src/pages/authors.js
+++ src/pages/authors.js
@@ -7,7 +7,8 @@ const AuthorsPage = () => (
     <h3>Writers</h3>
     <p>Taco</p>
     <p>Honey Butter</p>
-    <p>Naelotus</p>
+    <p>[Naelotus]</p>
+	<p>In a sustained “Flying at the seat of my pants” mode.  Similar to a wine aficionado, always looking for the next hot sauce and food pairing.  Pretend to be good at a healthy lifestyle by putting all current superfood and diet trends on a prize wheel and giving it a spin.  I decorate my walls with my own Painting With A Twist artwork and am unsure if this is self absorbed.  Bourbon.  So much bourbon.  Adventure Racing.  Keep me away from gear sales.  All my shaker bottles smell like fermented whey protein.  Gamer.  Love to be creative.  Love this project.  Love all the other authors except for one.</p>
     <p>Tubal-cain</p>
     <p>Gummiworms</p>
 
